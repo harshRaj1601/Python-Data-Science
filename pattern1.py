663ffa8c92a7fbec4bdd6c4619841b346721e1c7
Harsh Jaiswal <rajkumarkoylamandi@gmail.com>
2024-07-02 19:01:23 +0530

    #4 loops, for loop,

---
+++ pattern1.py
@@ -0,0 +1,33 @@
+from turtle import *
+
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+# fd(120)
+# rt(36)
+
+side = 15
+
+for i in range(side):
+    rt(360/side)
+    fd(120)
+    
+
+mainloop()
+
+#360/n
