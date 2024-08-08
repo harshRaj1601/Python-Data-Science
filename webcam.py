042ddaa30acdffb6c4e7a742362db9a62ae10728
Harsh Jaiswal <rajkumarkoylamandi@gmail.com>
2024-08-08 18:40:01 +0530

    # updated

--- webcam.py
+++ webcam.py
@@ -1,14 +1,16 @@
 import cv2
 
 cam = cv2.VideoCapture(0)
+cam1 = cv2.VideoCapture(1)
+
 c=0
-while cam.isOpened():
+while cam.isOpened() and cam1.isOpened():
     state, frame = cam.read()
-    if not state:
+    state1, frame1 = cam1.read()
+    if not state and not state1:
         break
-    # resize
     cv2.imshow("Webcam", frame)
-    print(frame.shape)
+    cv2.imshow("Webcam1", frame1)
     c+=1
     if cv2.waitKey(10) == ord("q"):
         break
